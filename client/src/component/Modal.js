18fd607ae41f47fa7c668a63f2c824af9fd074a3
mohamednasr20 <mohamednasr8687@gmail.com>
2021-01-10 15:04:16 -0500

    add modal component

---
+++ client/src/component/Modal.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDom from "react-dom";
+import history from "../history";
+
+const Modal = ({ title, content, actions }) => {
+  return ReactDom.createPortal(
+    <div
+      onClick={() => history.push("/")}
+      className="ui dimmer modals visible active"
+    >
+      <div
+        onClick={(e) => e.stopPropagation()}
+        className="ui standard modal visible active"
+      >
+        <div className="header">{title}</div>
+        <div className="content">{content}</div>
+        <div className="actions">{actions}</div>
+      </div>
+    </div>,
+    document.querySelector("#modal")
+  );
+};
+
+export default Modal;
--- client/src/component/streams/StreamDelete.js
+++ client/src/component/streams/StreamDelete.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Modal from "../Modal";
 
 const StreamDelete = () => {
-  return <div>StreamDelete</div>;
+  const actions = (
+    <>
+      <button className="ui negative button">Delete</button>
+      <button className="ui button">Cancel</button>
+    </>
+  );
+  return (
+    <div>
+      StreamDelete
+      <Modal
+        title="Delete Stream"
+        content="Are You Sure You want To Delete This Stream?"
+        actions={actions}
+      />
+    </div>
+  );
 };
 
 export default StreamDelete;
